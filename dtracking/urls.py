afbecbd25e922be1bb24edb425080bd81061d914
Christian Rojas <christiandiegor@gmail.com>
2015-11-19 18:19:39 -0300

    configuraciones url token

--- dtracking/urls.py
+++ dtracking/urls.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 
 
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 from emails.views import EmailViewSet
@@ -22,7 +23,7 @@ urlpatterns = [
 	# rutas de api rest
 	url(r'^api/', include(router.urls)),
 	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-	url(r'^api-token/', 'rest_framework.authtoken.views.obtain_auth_token'),
+	url(r'^api-token/', obtain_auth_token),
 	
 	# rutas de las paginas html del tracking
     url(r'^$', 'emails.views.dashboard'),
