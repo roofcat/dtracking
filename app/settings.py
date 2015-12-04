cad39c57f050656ad640f741abefabf9999134ab
Christian Rojas <christiandiegor@gmail.com>
2015-12-03 21:17:30 -0300

    django-debug-toolbar

--- app/settings.py
+++ app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # app de terceros
     'appengine_toolkit',
+    'django-debug-toolbar',
     'rest_framework',
     'rest_framework.authtoken',
     # apps
