5a758bade434f1405faf028b22e60f7cb01f77d9
Christian Rojas <christiandiegor@gmail.com>
2016-02-04 14:12:32 -0300

    cambio de datos de usuario en perfil

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
 
+import logging
+
+
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, QueryDict
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
@@ -21,7 +25,43 @@ class LoginRequiredMixin(object):
 class ProfileTemplateView(LoginRequiredMixin, TemplateView):
 
 	def get(self, request, *args, **kwargs):
+		""" Root del template del perfil
+		"""
 		return render(request, 'autenticacion/profile.html')
+
+	def post(self, request, *args, **kwargs):
+		""" Método para cambiar la contraseña del usuario
+		"""
+		try:
+			body = QueryDict(request.body)
+			user = User.objects.get(pk=request.user.id)
+			new_password1 = body.get('new_password1')
+			new_password2 = body.get('new_password2')
+			if new_password1 and new_password2 is not None:
+				user.set_password(new_password1)
+				user.save()
+				return HttpResponse('Contraseña cambiada.', content_type='application/json')
+			else:
+				return HttpResponse('No se pudo cambiar la contraseña.', content_type='application/json')
+		except Exception, e:
+			logging.error(e)
+			return HttpResponse(e)
+
+	def patch(self, request, *args, **kwargs):
+		""" Método para cambiar datos del usuario
+		"""
+		try:
+			body = QueryDict(request.body)
+			user = User.objects.get(pk=request.user.id)
+			first_name = body.get('first_name')
+			last_name = body.get('last_name')
+			user.first_name = first_name
+			user.last_name = last_name
+			user.save()
+			return HttpResponse('Registro actualizado.', content_type='application/json')
+		except Exception, e:
+			logging.error(e)
+			return HttpResponse(e)
 
 profile = ProfileTemplateView.as_view()
 
