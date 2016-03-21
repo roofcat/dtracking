cb505001f256b0e635778dc82ac300560edff0cb
Christian Rojas <christiandiegor@gmail.com>
2016-03-21 15:51:27 -0300

    webhook para unixtime 10

--- utils/generics.py
+++ utils/generics.py
@@ -9,10 +9,15 @@ tz = pytz.timezone("America/Santiago")
 
 
 def timestamp_to_date(x):
-    if x is not None:
-        if len(str(x)) > 10:
-            x = int(str(x)[0:10], base=10)
-        return datetime.fromtimestamp(x, tz=tz)
+	x = to_unix_timestamp(x)
+	return datetime.fromtimestamp(x, tz=tz)
+
+
+def to_unix_timestamp(x):
+	if x is not None:
+		if len(str(x)) > 10:
+			x = int(str(x)[0:10], base=10)
+		return x
 
 
 def get_date_from_timezone():
@@ -20,7 +25,7 @@ def get_date_from_timezone():
 
 
 def get_file_name_from_storage(name):
-    if name is not None:
-        name = name.split("/")
-        length = len(name) - 1
-        return name[length]
+	if name is not None:
+		name = name.split("/")
+		length = len(name) - 1
+		return name[length]
