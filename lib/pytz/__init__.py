ec5a4e89ccdfdf504a6edfca94f3fb034caa8ee4
Christian Rojas <christiandiegor@gmail.com>
2016-08-02 17:08:27 -0400

    libs

--- lib/pytz/__init__.py
+++ lib/pytz/__init__.py
@@ -9,8 +9,8 @@ on how to use these modules.
 '''
 
 # The IANA (nee Olson) database is updated several times a year.
-OLSON_VERSION = '2016c'
-VERSION = '2016.3'  # Switching to pip compatible version numbering.
+OLSON_VERSION = '2016d'
+VERSION = '2016.4'  # Switching to pip compatible version numbering.
 __version__ = VERSION
 
 OLSEN_VERSION = OLSON_VERSION # Old releases had this misspelling
@@ -848,6 +848,7 @@ all_timezones = \
  'Asia/Thimbu',
  'Asia/Thimphu',
  'Asia/Tokyo',
+ 'Asia/Tomsk',
  'Asia/Ujung_Pandang',
  'Asia/Ulaanbaatar',
  'Asia/Ulan_Bator',
@@ -974,6 +975,7 @@ all_timezones = \
  'Europe/Jersey',
  'Europe/Kaliningrad',
  'Europe/Kiev',
+ 'Europe/Kirov',
  'Europe/Lisbon',
  'Europe/Ljubljana',
  'Europe/London',
@@ -1404,6 +1406,7 @@ common_timezones = \
  'Asia/Tehran',
  'Asia/Thimphu',
  'Asia/Tokyo',
+ 'Asia/Tomsk',
  'Asia/Ulaanbaatar',
  'Asia/Urumqi',
  'Asia/Ust-Nera',
@@ -1462,6 +1465,7 @@ common_timezones = \
  'Europe/Jersey',
  'Europe/Kaliningrad',
  'Europe/Kiev',
+ 'Europe/Kirov',
  'Europe/Lisbon',
  'Europe/Ljubljana',
  'Europe/London',
